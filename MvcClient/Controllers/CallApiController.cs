b8ae9593da624154fbc51824df049dd8a242d378
jayroblack <jayroblack@gmail.com>
2020-02-10 11:15:32 -0600

    Adding ability to call the API from MVC

---
+++ MvcClient/Controllers/CallApiController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MvcClient.Controllers
+{
+    public class CallApiController : Controller
+    {
+        [HttpGet]
+        [Route("/api/call-api")]
+        public async Task<IActionResult> Index()
+        {
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            var content = await client.GetStringAsync("http://localhost:5001/identity");
+            var objAs = JsonConvert.DeserializeObject<List<Pair>>(content);
+            return Json(objAs);
+        }
+    }
+
+    public class Pair
+    {
+        public string Type { get; set; }
+        public string Value { get; set; }
+    }
+}
